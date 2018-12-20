15668b35e1f87ec1d4c7193e6761907dbe534d93
Brett Hartman <brett.hartman@mac.com>
2018-12-19 21:15:22 -0800

    Implement express router and configure basic routes

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
 
 const app = express();
+const db = require('./config/keys').mongoURI;
 
-app.get('/', (req, res) => {
-	res.send('Hello, World!');
-});
+mongoose
+	.connect(
+		db,
+		{ useNewUrlParser: true }
+	)
+	.then(() => {
+		console.log('Connected to the database.');
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
+
+// noinspection ES6ModulesDependencies
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
